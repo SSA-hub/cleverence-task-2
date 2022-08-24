620dc4798ea70a8ddd7eed9a5f804cbd09c60ee1
SSA-hub <ssapyat@gmail.com>
2022-08-24 23:32:32 +0300

    Initial commit

---
+++ AsyncCaller/AsyncCaller.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncCaller;
+
+public class AsyncCaller
+{
+    private readonly EventHandler _handler;
+    private Thread? _thread;
+
+    public AsyncCaller(EventHandler handler)
+    {
+        _handler = handler;
+    }
+
+    private static void Wait(object? timeout)
+    {
+        Thread.Sleep((int)timeout!);
+    }
+    
+    private void Aborter(IAsyncResult asyncResult)
+    {
+        _thread?.Abort();
+    }
+    
+    // EventHandler.BeginInvoke не поддерживается в текущей версии .NET, но
+    // я использовал его, т.к. он был в примере кода из задания
+    public bool Invoke(int timeout, object sender, EventArgs eventArgs)
+    {
+        _thread = new Thread(Wait);
+        var result = _handler?.BeginInvoke(sender, eventArgs, Aborter, this);
+        _thread.Start(timeout);
+        _thread.Join();
+        _thread = null;
+        return result is { IsCompleted: true };
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestConsole;
+
+class Program
+{
+    private static void TestEventHandler(object? sender, EventArgs e)
+    {
+        Console.WriteLine("Starting event handler");
+        Thread.Sleep(1000);
+        Console.WriteLine("Ending event handler");
+    }
+
+    private void CreateHandler()
+    {
+        var eventHandler = new EventHandler(TestEventHandler);
+        var asyncCaller = new global::AsyncCaller.AsyncCaller(eventHandler);
+        Console.WriteLine(asyncCaller.Invoke(5000, this, EventArgs.Empty) ? "Success!" : "Timeout!");
+    }
+    
+    public static void Main()
+    {
+        new Program().CreateHandler();
+        Console.WriteLine("End.");
+    }
+}
